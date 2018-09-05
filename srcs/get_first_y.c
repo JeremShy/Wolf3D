812da51395af5bb5a779d2b4427884c428b3509c
Jeremy CAMHI <jcamhi@student.42.fr>
2018-09-05 21:28:05 +0200

    get_first_y bug solved

--- srcs/get_first_y.c
+++ srcs/get_first_y.c
@@ -1,65 +1,77 @@
 #include <wolf3d.h>
 
-void get_first_y(t_data *data, t_vec3 ray_pos, t_vec3 ray_dir, t_hit_info *first_y)
+static void	get_first_inter(t_vec3 first_inter, t_vec3 ray_pos, t_vec3 ray_dir)
 {
-	int	map_pos[2];
-	t_vec3 first_inter;
 	double xb;
 	double ab;
-	double xc;
-	double bc;
-	t_vec3 delta_y;
-	t_vec3 actual_pos;
-
-	map_pos[0] = (int)ray_pos[0];
-	map_pos[1] = (int)ray_pos[1];
-	first_y->error = 1;
-
-	if (ray_dir[0] == 0)
-		return ;
-
 
 	xb = ray_dir[0] > 0 ? (int)(ray_pos[0]) + 1 : (int)ray_pos[0];
-
 	ab = (xb - ray_pos[0]) / ray_dir[0];
-	ft_vec3_init(first_inter, (double[]){
-		ab * ray_dir[0] + ray_pos[0],
-		ab * ray_dir[1] + ray_pos[1],
-		0.
-	});
+	ft_vec3_init(first_inter, (double[]){ab * ray_dir[0] + ray_pos[0],
+		ab * ray_dir[1] + ray_pos[1], 0.});
+}
 
-	xc = ray_dir[0] > 0 ? (int)(first_inter[0]) +	 1 : (int)(first_inter[0]) - 1;
+static void	get_delta_y(t_vec3 delta_y, t_vec3 ray_dir, t_vec3 first_inter)
+{
+	double xc;
+	double bc;
+
+	xc = ray_dir[0] > 0 ? (int)(first_inter[0]) + 1 :
+		(int)(first_inter[0]) - 1;
 	bc = (xc - first_inter[0]) / ray_dir[0];
+	ft_vec3_init(delta_y, (double[]){xc - first_inter[0], ray_dir[1] * bc, 0});
+}
 
-	ft_vec3_init(delta_y, (double[]){
-		xc - first_inter[0],
-		ray_dir[1] * bc,
-		0
-	});
-	ft_vec3_copy(actual_pos, first_inter);
+static void	east_west_collision(t_vec3 ap, t_hit_info *first_y, t_data *d)
+{
+	if (ap[0] - 1 >= 0 && d->map[(int)ap[0] - 1][(int)ap[1]].num != 0)
+	{
+		first_y->orientation = WEST;
+		ft_memcpy(first_y->collided_wall,
+			(int[2]){(int)ap[0] - 1, (int)ap[1]}, sizeof(int) * 2);
+	}
+	else
+	{
+		first_y->orientation = EAST;
+		ft_memcpy(first_y->collided_wall,
+			(int[2]){(int)ap[0], (int)ap[1]}, sizeof(int) * 2);
+	}
+}
+
+static void	boucle(t_vec3 first_inter, t_vec3 delta_y, t_data *d,
+	t_hit_info *first_y)
+{
+	t_vec3 ap;
+
+	ft_vec3_copy(ap, first_inter);
 	while (1)
 	{
-		if (actual_pos[0] < 0. || actual_pos[0] >= (double)data->size_y || actual_pos[1] < 0. || actual_pos[1] >= (double)data->size_x)
+		if (ap[0] < 0. || ap[0] >= (double)d->size_y
+			|| ap[1] < 0. || ap[1] >= (double)d->size_x)
 			return ;
-		if (   (actual_pos[0] - 1 >= 0 && data->map[(int)actual_pos[0] - 1 ][(int)actual_pos[1]].num != 0)
-				|| data->map[(int)actual_pos[0]			][(int)actual_pos[1]].num != 0)
+		if ((ap[0] - 1 >= 0 && d->map[(int)ap[0] - 1][(int)ap[1]].num != 0)
+			|| d->map[(int)ap[0]][(int)ap[1]].num != 0)
 		{
-			ft_vec3_copy(first_y->collision_pos, actual_pos);
+			ft_vec3_copy(first_y->collision_pos, ap);
 			first_y->side = 0;
 			first_y->error = 0;
-			if (actual_pos[0] - 1 >= 0 && data->map[(int)actual_pos[0] - 1][(int)actual_pos[1]].num != 0)
-			{
-				first_y->orientation = WEST;
-				ft_memcpy(first_y->collided_wall, (int[2]){(int)actual_pos[0] - 1, (int)actual_pos[1]}, sizeof(int) * 2);
-			}
-			else
-			{
-				first_y->orientation = EAST;
-				ft_memcpy(first_y->collided_wall, (int[2]){(int)actual_pos[0], (int)actual_pos[1]}, sizeof(int) * 2);
-			}
+			east_west_collision(ap, first_y, d);
 			return ;
 		}
-		ft_vec3_add(actual_pos, actual_pos, delta_y);
-
+		ft_vec3_add(ap, ap, delta_y);
 	}
+}
+
+void		get_first_y(t_data *data, t_vec3 ray_pos,
+	t_vec3 ray_dir, t_hit_info *first_y)
+{
+	t_vec3 first_inter;
+	t_vec3 delta_y;
+
+	first_y->error = 1;
+	if (ray_dir[0] == 0)
+		return ;
+	get_first_inter(first_inter, ray_pos, ray_dir);
+	get_delta_y(delta_y, ray_dir, first_inter);
+	boucle(first_inter, delta_y, data, first_y);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,36 @@
 #include <wolf3d.h>
 
-void load_texture(t_data *data, int nbr, char *file)
+static void	load_texture(t_data *d, int nbr, char *file)
 {
-		data->textures[nbr].img = mlx_xpm_file_to_image(data->mlx, file, &data->textures_size[nbr][0], &data->textures_size[nbr][1]);
-		data->textures[nbr].addr = mlx_get_data_addr(data->textures[nbr].img, &(data->textures[nbr].bpp), &(data->textures[nbr].size_line), &(data->textures[nbr].endian));
+	d->textures[nbr].img = mlx_xpm_file_to_image(d->mlx, file,
+		&d->textures_size[nbr][0], &d->textures_size[nbr][1]);
+	d->textures[nbr].addr = mlx_get_data_addr(d->textures[nbr].img,
+		&(d->textures[nbr].bpp), &(d->textures[nbr].size_line),
+		&(d->textures[nbr].endian));
 }
 
+static void	load_textures(t_data *data)
+{
+	load_texture(data, WOOD_TEXTURE, "textures/wood.xpm");
+	load_texture(data, STONE_TEXTURE, "textures/stone.xpm");
+	load_texture(data, RDASH_TEXTURE, "textures/rdash.xpm");
+	load_texture(data, TWILIGHT_TEXTURE, "textures/twilight.xpm");
+	load_texture(data, PASSAGE_SECRET_TEXTURE, "textures/passage_secret.xpm");
+}
 
-int main(int ac, char **av)
+static void	prepare_data_n_mlx(t_data *data)
+{
+	ft_vec3_init(data->cam_pos, (double[]){1.7, 9.1, 0});
+	ft_vec3_init(data->cam_dir, (double[]){0, 1, 0});
+	ft_vec3_init(data->cam_plane, (double[]){.5, 0, 0});
+	data->must_refresh = 1;
+	mlx_hook(data->win, 17, (1L << 17), red_cross_hook, data);
+	mlx_hook(data->win, 3, (1L << 1), key_release_hook, data);
+	mlx_hook(data->win, 2, (1L << 0), key_press_hook, data);
+	mlx_loop_hook(data->mlx, &loop_hook, data);
+}
+
+int			main(int ac, char **av)
 {
 	t_data	data;
 
@@ -23,31 +46,14 @@ int main(int ac, char **av)
 		ft_printf("%s: parse error\n", av[0]);
 		return (2);
 	}
-	ft_printf("size x : %d, size y : %d\n", data.size_x, data.size_y);
 	data.w = 512;
 	data.h = 512;
 	if (!init_the_mlx(&data))
 		return (0);
 	clear_image(data.img.img, data.h);
 	mlx_put_image_to_window(data.mlx, data.win, data.img.img, 0, 0);
-
-	load_texture(&data, WOOD_TEXTURE, "textures/wood.xpm");
-	load_texture(&data, STONE_TEXTURE, "textures/stone.xpm");
-	load_texture(&data, RDASH_TEXTURE, "textures/rdash.xpm");
-	load_texture(&data, TWILIGHT_TEXTURE, "textures/twilight.xpm");
-	load_texture(&data, PASSAGE_SECRET_TEXTURE, "textures/passage_secret.xpm");
-
-	ft_vec3_init(data.cam_pos, (double[]){1.7, 9.1, 0});
-
-	ft_vec3_init(data.cam_dir, (double[]){0, 1, 0});
-	ft_vec3_init(data.cam_plane, (double[]){.5, 0, 0});
-	data.must_refresh = 1;
-
-	// mlx_key_hook(data.win, &key_hook, &data);
-	mlx_hook(data.win, 17, (1L << 17), red_cross_hook, &data);
-	mlx_hook(data.win, 3, (1L << 1), key_release_hook, &data);
-	mlx_hook(data.win, 2, (1L << 0), key_press_hook, &data);
-	mlx_loop_hook(data.mlx, &loop_hook, &data);
+	load_textures(&data);
+	prepare_data_n_mlx(&data);
 	mlx_loop(data.mlx);
 	return (0);
 }
