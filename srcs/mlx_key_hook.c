bf38baff5bdb8582f817d4d596cb3e3b1a2a1676
Jeremy CAMHI <jcamhi@student.42.fr>
2018-09-06 18:28:34 +0200

    norme mlx_key_hook.c

--- srcs/mlx_key_hook.c
+++ srcs/mlx_key_hook.c
@@ -1,6 +1,42 @@
 #include <wolf3d.h>
 
-int	key_press_hook(int keycode, void *data_void)
+static void	pause_or_unpause(t_data *data)
+{
+	data->paused = (data->paused ? 0 : 1);
+	data->going_front = 0;
+	data->going_right = 0;
+	data->going_left = 0;
+	data->going_back = 0;
+	data->rotating_right = 0;
+	data->rotating_left = 0;
+	data->must_refresh = 1;
+}
+
+static void	handle_key(t_data *data, int keycode)
+{
+	if (keycode == 13)
+		data->going_front = 1;
+	else if (keycode == 1)
+		data->going_back = 1;
+	else if (keycode == 123)
+	{
+		data->rotating_left = 1;
+		data->rotating_right = 0;
+	}
+	else if (keycode == 124)
+	{
+		data->rotating_left = 0;
+		data->rotating_right = 1;
+	}
+	else if (keycode == 0)
+		data->going_left = 1;
+	else if (keycode == 2)
+		data->going_right = 1;
+	else if (keycode == 35)
+		pause_or_unpause(data);
+}
+
+int			key_press_hook(int keycode, void *data_void)
 {
 	t_data	*data;
 
@@ -12,57 +48,27 @@ int	key_press_hook(int keycode, void *data_void)
 		free_mlx_and_exit(data);
 	}
 	if (!data->paused || keycode == 35)
-	{
-		if (keycode == 13) // W
-			data->going_front = 1;
-		else if (keycode == 1) // S
-			data->going_back = 1;
-		else if (keycode == 123) // A
-		{
-			data->rotating_left = 1;
-			data->rotating_right = 0;
-		}
-		else if (keycode == 124) // D
-		{
-			data->rotating_left = 0;
-			data->rotating_right = 1;
-		}
-		else if (keycode == 0) // Q
-			data->going_left = 1;
-		else if (keycode == 2) // E
-			data->going_right = 1;
-		else if (keycode == 35) // P
-		{
-			data->paused = (data->paused ? 0 : 1);
-			data->going_front = 0;
-			data->going_right = 0;
-			data->going_left = 0;
-			data->going_back = 0;
-			data->rotating_right = 0;
-			data->rotating_left = 0;
-			data->must_refresh = 1;
-		}
-	}
+		handle_key(data, keycode);
 	return (1);
 }
 
-int	key_release_hook(int keycode, void *data_void)
+int			key_release_hook(int keycode, void *data_void)
 {
 	t_data	*data;
 
 	data = (t_data*)data_void;
 	ft_printf("released key : %d\n", keycode);
-	if (keycode == 13) // W
+	if (keycode == 13)
 		data->going_front = 0;
-	else if (keycode == 1) // S
+	else if (keycode == 1)
 		data->going_back = 0;
-	else if (keycode == 123) // A
+	else if (keycode == 123)
 		data->rotating_left = 0;
-	else if (keycode == 124) // D
+	else if (keycode == 124)
 		data->rotating_right = 0;
-	else if (keycode == 0) // Q
+	else if (keycode == 0)
 		data->going_left = 0;
-	else if (keycode == 2) // E
+	else if (keycode == 2)
 		data->going_right = 0;
 	return (1);
 }
